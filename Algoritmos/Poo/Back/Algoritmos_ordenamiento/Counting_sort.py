a1b8b74fb62af939a9d563437677fe3bafb1e137
Daniel Morales Toro <83548789+Vega3@users.noreply.github.com>
2024-05-05 09:58:54 -0500

    Counting_sort.py analisis big O

--- Algoritmos/Poo/Back/Algoritmos_ordenamiento/Counting_sort.py
+++ Algoritmos/Poo/Back/Algoritmos_ordenamiento/Counting_sort.py
@@ -3,16 +3,16 @@ from Algoritmos.Poo.Back import Metodo_de_ordenamiento
 
 class CountingSort(Metodo_de_ordenamiento.MetodoOrdenamiento):
     @staticmethod
-    def sort(arr):
-        max_value = max(arr)
-        min_value = min(arr)
-        count = [0] * (max_value - min_value + 1)
+    def sort(arr):    # O(n+k)
+        max_value = max(arr)    # O(n)
+        min_value = min(arr)    # O(n)
+        count = [0] * (max_value - min_value + 1)    # O(k)
 
-        for num in arr:
-            count[num - min_value] += 1
+        for num in arr:    # O(n)
+            count[num - min_value] += 1    # O(1)
 
-        sorted_arr = []
-        for i in range(len(count)):
-            sorted_arr.extend([i + min_value] * count[i])
+        sorted_arr = []    # O(1)
+        for i in range(len(count)):    # O(k)
+            sorted_arr.extend([i + min_value] * count[i])    # O(n)
 
-        return sorted_arr
+        return sorted_arr    # O(1)
