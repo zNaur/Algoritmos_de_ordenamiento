15efc90716f2ab940f9572dae73d823cda9bfe08
Daniel Morales Toro <83548789+Vega3@users.noreply.github.com>
2024-05-05 09:29:13 -0500

    Bubble_sort.py con analisis big o

--- Algoritmos/Poo/Back/Algoritmos_ordenamiento/Bubble_sort.py
+++ Algoritmos/Poo/Back/Algoritmos_ordenamiento/Bubble_sort.py
@@ -3,11 +3,10 @@ from Algoritmos.Poo.Back import Metodo_de_ordenamiento
 
 class BubbleSort(Metodo_de_ordenamiento.MetodoOrdenamiento):
     @staticmethod
-    def sort(arr):
-        n = len(arr)
-        for i in range(n - 1):
-            for j in range(0, n - i - 1):
-                if arr[j] > arr[j + 1]:
-                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        return arr
-
+    def sort(arr):                # O(n^2)
+        n = len(arr)                # O(1)
+        for i in range(n - 1):            # O(n)
+            for j in range(0, n - i - 1):        #O(n^2)
+                if arr[j] > arr[j + 1]:            # O(1)
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]        # O(1)
+        return arr            # O(1)
